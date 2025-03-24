148f3daa9e32437ace1546f9ffd09c8172cb69db
Ratthapit <pimpitcha_kanitpanya@cmu.ac.th>
2025-03-24 16:06:15 +0700

    แก้ชื่อ Method

--- src/Utilities/Direction.java
+++ src/Utilities/Direction.java
@@ -3,30 +3,30 @@ package com.example.demo.src.Utilities;
 public class Direction {
     private static final int BOARD_SIZE = 8;
 
-    public static int[] getTop(int row, int col) {
+    public static int[] up(int row, int col) {
         return (row > 0) ? new int[]{row - 1, col} : new int[]{-1, -1};
     }
 
-    public static int[] getBottom(int row, int col) {
+    public static int[] down(int row, int col) {
         return (row < BOARD_SIZE - 1) ? new int[]{row + 1, col} : new int[]{-1, -1};
     }
 
-    public static int[] getTopLeft(int row, int col, boolean isEven) {
+    public static int[] upleft(int row, int col, boolean isEven) {
         if (col == 0) return new int[]{-1, -1};
         return (!isEven && row > 0) ? new int[]{row - 1, col - 1} : new int[]{row, col - 1};
     }
 
-    public static int[] getBottomLeft(int row, int col, boolean isEven) {
+    public static int[] downleft(int row, int col, boolean isEven) {
         if (col == 0) return new int[]{-1, -1};
         return (isEven && row < BOARD_SIZE - 1) ? new int[]{row + 1, col - 1} : new int[]{row, col - 1};
     }
 
-    public static int[] getTopRight(int row, int col, boolean isEven) {
+    public static int[] upright(int row, int col, boolean isEven) {
         if (col == BOARD_SIZE - 1) return new int[]{-1, -1};
         return (!isEven && row > 0) ? new int[]{row - 1, col + 1} : new int[]{row, col + 1};
     }
 
-    public static int[] getBottomRight(int row, int col, boolean isEven) {
+    public static int[] downright(int row, int col, boolean isEven) {
         if (col == BOARD_SIZE - 1) return new int[]{-1, -1};
         return (isEven && row < BOARD_SIZE - 1) ? new int[]{row + 1, col + 1} : new int[]{row, col + 1};
     }
